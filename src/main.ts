3fad56b56958449e634792b54720650d96dcfc77
Leonard Speer <lspeer@exa-online.de>
2023-02-05 19:11:17 +0100

    LM-118 Add vue-scroll-to to main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,16 @@
 import { createApp } from 'vue';
+import VueScrollTo from 'vue-scrollto';
 import App from './App.vue';
 import './registerServiceWorker';
 import router from './router';
 import store from './store';
 
-createApp(App).use(store).use(router).mount('#app');
+createApp(App)
+.use(store)
+.use(router)
+.use(VueScrollTo, {
+    duration: 800,
+    easing: 'ease-in-out',
+    force: true,
+})
+.mount('#app');
