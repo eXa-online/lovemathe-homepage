dec1078fb3bdb5ad098c0f5be8457bee46c27103
Leonard Speer <lspeer@exa-online.de>
2023-03-09 17:28:16 +0100

    LM-124 Add Imprint component and view

---
+++ src/assets/markdown/imprint/imprint_left.md
@@ -0,0 +1,18 @@
+Verantwortlich für Inhalt und Umsetzung von eXa-online.de:
+
+
+eXa-online GmbH
+
+Kochstraße 138
+
+04277 Leipzig
+
+[Tel.: +49 341 8781754](tel:+493418781754)
+
+Fax: +49 341 8781755
+
+[info[at]exa-online.de](mailto:info@exa-online.de)
+
+Geschäftsführer:
+
+Carsten Paul
---
+++ src/components/TwoRowsText.vue
@@ -0,0 +1,131 @@
+<template>
+  <div class="two-rows" :style="cssProps">
+    <h3 class="two-rows--heading">{{ heading }}</h3>
+    <div class="row two-rows--text">
+      <div class="two-rows--text__body">
+        <vue-markdown :source="text1" />
+      </div>
+    </div>
+    <div class="row two-rows--text">
+      <div class="two-rows--text__body">
+        <vue-markdown :source="text2" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import VueMarkdown from 'vue-markdown-render';
+
+export default defineComponent({
+  name: 'TwoRowsWithImage',
+  components: {
+    VueMarkdown,
+  },
+  data() {
+    return {
+      text1: require(`../assets/markdown/${this.textSourceLeft}`).default,
+      text2: require(`../assets/markdown/${this.textSourceRight}`).default,
+    };
+  },
+  props: {
+    heading: {
+      required: false,
+      type: String,
+    },
+    textSourceLeft: {
+      required: true,
+      type: String,
+    },
+    textSourceRight: {
+      required: true,
+      type: String,
+    },
+    flexDirection: {
+      required: true,
+      type: String,
+    },
+  },
+  computed: {
+    cssProps() {
+      return {
+        '--flex-direction': this.flexDirection,
+      };
+    },
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.row {
+  width: 100%;
+
+  @media #{$mobile} {
+    margin-left: 0;
+    margin-right: 0;
+    width: 100%;
+  }
+}
+
+.two-rows {
+  display: flex;
+  width: 80%;
+  justify-content: space-between;
+  align-items: flex-start;
+  align-content: space-between;
+  flex-direction: var(--flex-direction);
+  padding-top: 2rem;
+
+  @media #{$mobile} {
+    flex-direction: column;
+    width: 100%;
+    margin-left: 0;
+    margin-right: 0;
+    padding-top: 0;
+  }
+}
+
+.two-rows--image {
+  max-width: 100%;
+
+  @media #{$mobile} {
+    max-width: 80%;
+    padding-top: 1rem;
+  }
+}
+
+.two-rows--text {
+  display: flex;
+  flex-direction: column;
+  max-width: 80%;
+
+  @media #{$mobile} {
+    max-width: 100%;
+  }
+}
+
+.two-rows--text__heading {
+  font-family: Kiwi-Days;
+  font-weight: 400;
+  color: #0d5b67;
+  font-size: 1.8rem;
+
+  @media #{$mobile} {
+    font-size: 1.2rem;
+  }
+}
+
+.two-rows--text__body {
+  font-family: Rubik;
+  font-weight: 400;
+  color: #0d5b67;
+  font-size: 1.1rem;
+  text-align: left;
+  line-height: 1rem;
+
+  @media #{$mobile} {
+    font-size: 0.9rem;
+  }
+}
+</style>
