1035810080ecdfa2a9f0bb0c00a43b31aa7ce46a
Leonard Speer <lspeer@exa-online.de>
2023-03-09 17:28:16 +0100

    LM-124 Add Imprint component and view

---
+++ src/assets/markdown/imprint/imprint_right.md
@@ -0,0 +1,17 @@
+Registergericht:
+
+Amtsgericht Leipzig HRB 15180
+
+
+Sitz der Gesellschaft:
+
+eXa-online GmbH
+
+Kochstraße 138
+
+04277 Leipzig
+
+
+Ust.-Id.Nr. gemäß §27a Umsatzsteuergesetz:
+
+DE-197342272 
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
 import Home from '../views/Home.vue';
+import Imprint from '../views/Imprint.vue';
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     name: 'Home',
     component: Home,
+  },
+  {
+    path: '/impressum',
+    name: 'Imprint',
+    component: Imprint,
   },
 ];
 
