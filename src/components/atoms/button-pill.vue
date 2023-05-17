c469886bdaafa2588dd790ee7bbd82d8149f850d
Daniel Weiß <dweiss@exa-online.de>
2023-05-17 07:48:43 +0200

    LM-149 Add repeating style elements

---
+++ src/components/atoms/button-pill.vue
@@ -0,0 +1,46 @@
+<template>
+  <button class="button" :class="{ capitalize: props.capitalize, uppercase: props.uppercase }">
+    <slot />
+  </button>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  capitalize?: boolean,
+  uppercase?: boolean,
+}>()
+</script>
+
+<style lang="scss" scoped>
+.button {
+  position: relative;
+  background-color: #{$cta-background-color};
+  border: none;
+  border-radius: 50px;
+  color: white;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  margin-top: 1rem;
+  padding: 1.2rem 2.4rem;
+  cursor: pointer;
+
+  font-family: 'Rubik';
+  font-style: normal;
+  font-weight: 500;
+  font-size: 24px;
+  line-height: 120.28%;
+
+  &.capitalize {
+    text-transform: capitalize;
+  }
+  &.uppercase {
+    text-transform: uppercase;
+  }
+
+  @media #{$mobile} {
+    font-size: 1.2rem;
+    padding: 0.8rem;
+  }
+}
+</style>
