ad8b750ccdaa264f3b89ac7c45cfaff5b235a1ef
Daniel Weiß <dweiss@exa-online.de>
2023-06-01 10:07:19 +0200

    LM-149 Add new old impressum page

--- src/pages/impressum.vue
+++ src/pages/impressum.vue
@@ -1,31 +1,67 @@
 <template>
   <div class="imprint">
-    <ImprintBody />
-    <Footer />
+    <h1>Impressum</h1>
+    <div class="imprint-data">
+      <div class="imprint-data__column">
+        <p>Verantwortlich für Inhalt und Umsetzung von eXa-online.de:</p>
+        <p>eXa-online GmbH</p>
+        <p>Kochstraße 138</p>
+        <p>04277 Leipzig</p>
+        <p><a href="tel:+493418781754">Tel.:+49 341 8781754</a></p>
+        <p>Fax: +49 341 8781755</p>
+        <p><a href="mailto:info@exa-online.de">info[at]exa-online.de</a></p>
+        <p>Geschäftsführer:</p>
+        <p>Carsten Paul</p>
+      </div>
+      <div class="imprint-data__column">
+        <p>Registergericht:</p>
+        <p>Amtsgericht Leipzig HRB 15180</p>
+        <p>Sitz der Gesellschaft:</p>
+        <p>eXa-online GmbH</p>
+        <p>Kochstraße 138</p>
+        <p>04277 Leipzig</p>
+        <p>Ust.-Id.Nr. gemäß §27a Umsatzsteuergesetz:</p>
+        <p>DE-197342272</p>
+      </div>
+    </div>
+    <atoms-button-pill @click="() => router.push({ path: '/' })">
+      Zurück zur Startseite
+    </atoms-button-pill>
   </div>
 </template>
 
 <script setup lang="ts">
-import ImprintBody from '@/components/ImprintBody.vue';
-import Footer from '@/components/Footer.vue';
+import { defineComponent } from 'vue'
+const router = useRouter()
 </script>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-
-export default defineComponent({
-  name: 'Imprint',
-});
 </script>
 
 <style lang="scss" scoped>
   .imprint {
+    padding-top: calc(2rem + var(--header-height));
+    padding-bottom: 2rem;
     display: flex;
     flex-direction: column;
-    height: 100vh;
+    justify-content: flex-start;
+    align-items: center;
+    min-height: calc(100vh - var(--footer-height));
+    box-sizing: border-box;
+  }
+  .imprint-data {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
 
-    @media #{$mobile} {
-      min-height: 130vh;
+    @media #{$tab}, #{$desk}, #{$big} {
+      flex-direction: row;
     }
+
+    justify-content: space-evenly;
+  }
+
+  .imprint-data__column {
+    padding: 0 1rem;
   }
 </style>
