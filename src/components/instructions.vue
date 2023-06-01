ce91183477f76602de66f71f3f7dbf5d77df35a4
Daniel Weiß <dweiss@exa-online.de>
2023-06-01 10:16:26 +0200

    LM-149 Add new instructions section

---
+++ src/components/instructions.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="instruction">
+    <h2>Anleitung</h2>
+    <instructions-row title="Test&shy;anleitung" markdown="instruction/test_instruction.md" />
+    <instructions-row
+      title="Auswertung"
+      markdown="instruction/evaluation.md"
+      inverted
+      button-text="Zum Cheatsheet"
+    >
+      <template #lightbox>
+        <img src="~/assets/img/test_instruction.webp" alt="test instructions">
+      </template>
+    </instructions-row>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style lang="scss" scoped>
+#instruction {
+  margin: 4rem 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  width: 100%;
+}
+</style>
