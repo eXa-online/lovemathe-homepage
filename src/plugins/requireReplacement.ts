c34b661c2cbab2730236268ff18b133e3f8053b2
Daniel Weiß <dweiss@exa-online.de>
2023-06-01 10:09:05 +0200

    LM-149 Reenable and update linting

--- src/plugins/requireReplacement.ts
+++ src/plugins/requireReplacement.ts
@@ -2,35 +2,29 @@ export default defineNuxtPlugin(() => {
   const markdowns = import.meta.glob('~/assets/markdown/**/*.md', {
     import: 'default',
     eager: true,
-    as: "raw",
+    as: 'raw'
   })
 
   const images = import.meta.glob('~/assets/img/**/*', {
     import: 'default',
     eager: true,
-    as: "url",
+    as: 'url'
   })
 
   const videos = import.meta.glob('~/assets/videos/**/*', {
     import: 'default',
     eager: true,
-    as: "url",
+    as: 'url'
   })
 
   return {
     provide: {
-      requireMarkdown: (path: string) => {
-        return markdowns[`/assets/markdown/${path}`]
-      },
-      requireImg: (path: string) => {
-        return images[`/assets/img/${path}`]
-      },
-      requireVideo: (path: string) => {
-        return { 
-          desktop: videos[`/assets/videos/${path}/big.mp4`],
-          mobile: videos[`/assets/videos/${path}/small.mp4`],
-        }
-      },
+      requireMarkdown: (path: string) => markdowns[`/assets/markdown/${path}`],
+      requireImg: (path: string) => images[`/assets/img/${path}`],
+      requireVideo: (path: string) => ({
+        desktop: videos[`/assets/videos/${path}/big.mp4`],
+        mobile: videos[`/assets/videos/${path}/small.mp4`]
+      })
     }
   }
 })
