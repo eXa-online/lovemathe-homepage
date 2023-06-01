3a28789d961e58a798b805574fe53e4b0cb86c9e
Daniel Weiß <dweiss@exa-online.de>
2023-06-01 10:17:31 +0200

    LM-149 Add new literature cta

---
+++ src/components/literature.vue
@@ -0,0 +1,27 @@
+<template>
+  <atoms-content-card id="literature">
+    <h2 class="smaller">
+      Literatur
+    </h2>
+    <atoms-lightbox button-text="Mehr">
+      <vue-markdown style="{ text-align: left !important; }" :source="content" />
+    </atoms-lightbox>
+  </atoms-content-card>
+</template>
+
+<script setup lang="ts">
+import VueMarkdown from 'vue-markdown-render'
+import { useMarkdownImport } from '~/composables/use-requires'
+
+const requireMarkdown = useMarkdownImport()
+
+const content = requireMarkdown('documentation/literature.md')
+</script>
+
+<style lang="scss" scoped>
+#literature {
+  margin: 4rem 0;
+  @extend .switching-flex-direction;
+  justify-content: space-between;
+}
+</style>
