bef89327c0ee63d2d0094f67183f19066be36177
Daniel Weiß <dweiss@exa-online.de>
2023-06-01 10:15:14 +0200

    LM-149 Add new about us section

---
+++ src/components/about.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="about">
+    <h2>Über das Projekt</h2>
+    <about-row title="Die Idee" markdown="about_us/idea.md" />
+    <about-row title="Ferry Porsche Challenge" markdown="about_us/ferry_porsche_challenge.md" />
+    <about-row title="Die Relevanz" markdown="about_us/relevance.md" />
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style lang="scss" scoped>
+#about {
+  margin: 4rem 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  width: 100%;
+
+  p {
+    text-align: start;
+  }
+
+  h2 {
+    margin-bottom: 2rem; 
+    @media #{$desk} {
+      margin-bottom: 4rem; 
+    }
+  }
+}
+</style>
