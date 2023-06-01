cfd69e5e98a8ec96c545d374048a975a43a9adce
Daniel Weiß <dweiss@exa-online.de>
2023-06-01 10:14:31 +0200

    LM-149 Add new test game card section

---
+++ src/components/games-section.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="games-section">
+    <h3>{{ props.title }}</h3>
+    <vue-markdown v-if="props.markdown" :source="props.markdown" />
+    <p v-else>
+      {{ props.text || "" }}
+    </p>
+    <slot />
+  </div>
+</template>
+
+<script setup lang="ts">
+import VueMarkdown from 'vue-markdown-render'
+
+const props = defineProps<{
+  title: string,
+  text?: string,
+  markdown?: string,
+}>()
+</script>
+
+<style lang="scss" scoped>
+.games-section {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+}
+</style>
